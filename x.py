0e8b418f858d0e09942e9ba175002c2cbb22dbb1
asso-2020 <67026951+asso-2020@users.noreply.github.com>
2021-04-23 06:02:37 +0300

    Update x.py

--- x.py
+++ x.py
@@ -3,11 +3,11 @@ import serial
 
 arduino = serial.Serial('COM9', 115200, timeout=.1)
 
-def blink():
+def blinkLed():
   while True:
     arduino.write(b'H')
     time.sleep(1)
     arduino.write(b'L')
     time.sleep(1)
 
-blink()
+blinkLed()
